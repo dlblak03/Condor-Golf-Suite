83d4df9b2a9991b1e9ebce029e61a51a449e43f5
dlblak03 <daltonb0599@gmail.com>
2021-09-10 15:03:15 -0400

    Updates

--- AngularWebApp/src/app/pages/dashboard/dashboard.component.ts
+++ AngularWebApp/src/app/pages/dashboard/dashboard.component.ts
@@ -7,30 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  WeatherData: any;
+  WeatherDataCurrent: any;
+  WeatherDataHourly: any;
+
   constructor() { }
 
   ngOnInit() {
     this.getWeatherData();
-    console.log(this.WeatherData);
+    console.log(this.WeatherDataCurrent);
   }
 
   getWeatherData() {
-    let data = JSON.parse('{"coord":{"lon":-85.7594,"lat":38.2542},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":297.24,"feels_like":296.91,"temp_min":295.41,"temp_max":298.18,"pressure":1014,"humidity":46},"visibility":10000,"wind":{"speed":0.89,"deg":322,"gust":1.34},"clouds":{"all":59},"dt":1631228338,"sys":{"type":2,"id":2009252,"country":"US","sunrise":1631186393,"sunset":1631232060},"timezone":-14400,"id":4299276,"name":"Louisville","cod":200}');
-    this.setWeatherData(data);
+    let data = JSON.parse('{"coord":{"lon":-85.7594,"lat":38.2542},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":298.82,"feels_like":298.67,"temp_min":296.52,"temp_max":300.31,"pressure":1019,"humidity":47},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":90},"dt":1631299129,"sys":{"type":2,"id":2010269,"country":"US","sunrise":1631272844,"sunset":1631318366},"timezone":-14400,"id":4299276,"name":"Louisville","cod":200}');
+    this.setWeatherDataCurrent(data);
   }
 
-  setWeatherData(data: any) {
-    this.WeatherData = data;
-    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
-    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    let currentData = new Date();
-    this.WeatherData.isDay = (currentData.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
-    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    this.WeatherData.temp_feels = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.humidity = this.WeatherData.main.humidity;
+  setWeatherDataCurrent(data: any) {
+    this.WeatherDataCurrent = data;
+    let sunsetTime = new Date(this.WeatherDataCurrent.sys.sunset * 1000);
+    this.WeatherDataCurrent.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate: Date = new Date();
+    this.WeatherDataCurrent.isDay = (currentDate.getTime() < sunsetTime.getTime());
+
+    this.WeatherDataCurrent.temp_celcius = (this.WeatherDataCurrent.main.temp - 273.15).toFixed(0);
+    this.WeatherDataCurrent.temp_min_celcius = (this.WeatherDataCurrent.main.temp_min - 273.15).toFixed(0);
+    this.WeatherDataCurrent.temp_max_celcius = (this.WeatherDataCurrent.main.temp_max - 273.15).toFixed(0);
+    this.WeatherDataCurrent.temp_feels_celcius = (this.WeatherDataCurrent.main.feels_like - 273.15).toFixed(0);
+
+    this.WeatherDataCurrent.temp_fahrenheit = (this.WeatherDataCurrent.temp_celcius * (9/5)) + 32;
+    this.WeatherDataCurrent.temp_min_fahrenheit = (this.WeatherDataCurrent.temp_min_celcius * (9/5)) + 32;
+    this.WeatherDataCurrent.temp_max_fahrenheit = (this.WeatherDataCurrent.temp_max_celcius * (9/5)) + 32;
+    this.WeatherDataCurrent.temp_feels_fahrenheit = (this.WeatherDataCurrent.temp_feels_celcius * (9/5)) + 32;
+
+    this.WeatherDataCurrent.humidity = this.WeatherDataCurrent.main.humidity;
+    this.WeatherDataCurrent.wind = this.WeatherDataCurrent.wind.speed * 2.23694;
+
+    this.WeatherDataCurrent.day = currentDate.toLocaleDateString('en-us', { weekday: 'long' });
+    this.WeatherDataCurrent.time = this.roundToHour(currentDate).toLocaleTimeString('en-us', {hour: 'numeric', minute:'2-digit'});
+    this.WeatherDataCurrent.description = this.WeatherDataCurrent.weather[0].description;
+  }
+
+  roundToHour(date: Date) {
+    let p = 60 * 60 * 1000; // milliseconds in an hour
+    return new Date(Math.round(date.getTime() / p) * p);
   }
 
 }
