d2f5ea09214324d39504c23d735813c3bfa8f31a
Dalton <daltonb0599@gmail.com>
2021-09-14 21:58:06 -0400

    Updates

--- AngularWebApp/src/app/pages/dashboard/dashboard.component.ts
+++ AngularWebApp/src/app/pages/dashboard/dashboard.component.ts
@@ -18,6 +18,10 @@ export class DashboardComponent implements OnInit {
   }
 
   getWeatherData() {
+    // fetch('https://api.openweathermap.org/data/2.5/weather?q=louisville&appid=0c27bfb17162df596beb01d7f2b5cf65')
+    // .then(response=>response.json())
+    // .then(data=>{this.setWeatherDataCurrent(data);})
+
     let data = JSON.parse('{"coord":{"lon":-85.7594,"lat":38.2542},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":298.82,"feels_like":298.67,"temp_min":296.52,"temp_max":300.31,"pressure":1019,"humidity":47},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":90},"dt":1631299129,"sys":{"type":2,"id":2010269,"country":"US","sunrise":1631272844,"sunset":1631318366},"timezone":-14400,"id":4299276,"name":"Louisville","cod":200}');
     this.setWeatherDataCurrent(data);
   }
@@ -40,7 +44,7 @@ export class DashboardComponent implements OnInit {
     this.WeatherDataCurrent.temp_feels_fahrenheit = (this.WeatherDataCurrent.temp_feels_celcius * (9/5)) + 32;
 
     this.WeatherDataCurrent.humidity = this.WeatherDataCurrent.main.humidity;
-    this.WeatherDataCurrent.wind = this.WeatherDataCurrent.wind.speed * 2.23694;
+    this.WeatherDataCurrent.wind = (this.WeatherDataCurrent.wind.speed * 2.23694).toFixed(2);
 
     this.WeatherDataCurrent.day = currentDate.toLocaleDateString('en-us', { weekday: 'long' });
     this.WeatherDataCurrent.time = this.roundToHour(currentDate).toLocaleTimeString('en-us', {hour: 'numeric', minute:'2-digit'});
