5fad84df342037664af1f8b22d2028af0cac190c
Dalton <daltonb0599@gmail.com>
2021-11-04 23:53:39 -0400

    Updates

--- AngularWebApp/src/app/pages/players/players.component.ts
+++ AngularWebApp/src/app/pages/players/players.component.ts
@@ -10,6 +10,13 @@ export class PlayersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.getAllPlayers();
+  }
+
+  getAllPlayers() {
+    fetch('https://kw31bx8r49.execute-api.us-east-1.amazonaws.com/players')
+    .then(response=>response.json())
+    .then(data=>{});
   }
 
 }
