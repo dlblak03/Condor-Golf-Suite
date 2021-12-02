6663618ce76aface65d1d677f74979308df9cf15
dlblak03 <daltonb0599@gmail.com>
2021-12-02 17:16:16 -0500

    Updates

--- AngularWebApp/src/app/pages/players/players.component.ts
+++ AngularWebApp/src/app/pages/players/players.component.ts
@@ -4,6 +4,9 @@ import { AuthUserService } from 'src/app/auth/auth-user.service';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable, ReplaySubject } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AddPlayerComponent } from '../../dialogs/add-player/add-player.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export interface Player {
   playerid: string;
@@ -18,15 +21,16 @@ export interface Player {
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
+  durationInSeconds = 2;
 
-  displayedColumns: string[] = ['playerid', 'name', 'email', 'phone'];
+  displayedColumns: string[] = ['playerid', 'name', 'email', 'phone', 'delete'];
   dataSource = new ExampleDataSource([]);
   sorter: any = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   players: Player[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
 
-  constructor(private http: HttpClient, private authUser: AuthUserService) { }
+  constructor(private http: HttpClient, private authUser: AuthUserService, public dialog: MatDialog, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.sortTable('A');
@@ -55,6 +59,25 @@ export class PlayersComponent implements OnInit {
               this.dataSource.paginator = this.paginator;
               console.log(this.players);
             });
+  }
+
+  openAddPlayer(): void {
+    this.dialog.open(AddPlayerComponent, {
+      width: '325px'
+    });
+  }
+
+  async deletePlayer(playerid: any) {
+    const headers = { 'headers': {'Access-Control-Allow-Origin': 'http://localhost:4200, https://master.d4pza09saklb.amplifyapp.com',
+      'Access-Control-Allow-Methods': 'http://localhost:4200, https://master.d4pza09saklb.amplifyapp.com',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret',
+      'Authorization': 'Bearer ' + this.authUser.getJwtToken()
+    }};
+    const body = { playerid: playerid };
+    this.http.delete('https://kw31bx8r49.execute-api.us-east-1.amazonaws.com/players/' + playerid, headers)
+        .subscribe(() => {
+          console.log("fuck you");
+        });
   }
 
 }
